ad43f55ec81ddef03c36a533de4135991c97383c
VijinV <vijinvinod.dev@gmail.com>
2026-07-07 12:58:00 +0530

    fix: increase sqlite connection pool max to prevent concurrent request deadlock

--- config/database.ts
+++ config/database.ts
@@ -49,6 +49,8 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database 
       },
       useNullAsDefault: true,
       pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 40),
         afterCreate: (conn: any, cb: any) => {
           try {
             conn.pragma('journal_mode = WAL');
